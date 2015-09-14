69546ad06ea7a572531b67761387a8d6a4e4e7ef
Treyphor <joshuahansen@live.com>
2015-09-13 22:06:06 -0600

    Commit 4-3
    Wired collection into postsList template.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,6 @@
 if (Posts.find().count() === 0) {
 	Posts.insert({
-		title: 'Introducting Telescope',
+		title: 'Introducing Telescope',
 		url: 'http://sachagreif.com/introducing-telescope/'
 	});
 
